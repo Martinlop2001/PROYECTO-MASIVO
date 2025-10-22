4085e76285663c4c6a9d8627b812c8a39d31b892
Luis <seivaneluis@gmail.com>
2025-10-22 16:51:42 -0300

    Creacción de la Ventana test (principal).

--- src/test/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-import sys
-from test_ventana import VentanaPrincipal
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    ventana = VentanaPrincipal()
-    ventana.show()
-    sys.exit(app.exec_())
